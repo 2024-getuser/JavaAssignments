dc6576b68dc1fef07c2102f5aa37739f384e4c84
Your Name <githubtest2024@gmail.com>
2024-12-31 14:57:42 +0530

    LoginPageTest updated

--- GIthubPractice/src/tests/LoginPageTest.java
+++ GIthubPractice/src/tests/LoginPageTest.java
@@ -1,11 +1,14 @@
 package tests;
 
 public class LoginPageTest {
-<<<<<<< HEAD
+
 	public void test1() {
 		System.out.println("Login Page test1  - LP");
+		String un ="admin";
+		String pwd = "admin";
+		System.out.println("Logged in ..... ");
 	}
-=======
->>>>>>> 0cb27f51cf4783b3c06e53701d8211e1895a82bf
+
+
 
 }
